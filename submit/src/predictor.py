d42a1758c69f0b80c144a28381055e03bfd29820
Yoshitaka Onozato <onozato822@gmail.com>
2024-03-16 20:49:36 +0900

    モデルファイルを読み込んで予測できるように修正

--- submit/src/predictor.py
+++ submit/src/predictor.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import pickle
+import os
+import lightgbm
 
 class ScoringService(object):
     @classmethod
@@ -13,7 +16,15 @@ class ScoringService(object):
         Returns:
             bool: The return value. True for success.
         """
-        cls.model = None
+        
+        # model_path = '../model/model.pkl'
+        model_path = os.path.join(model_path, 'model.pkl')
+
+        print(model_path)
+        with open(model_path, 'rb') as f:
+            cls.model = pickle.load(f)
+        
+        # cls.model = None
         cls.data = inference_df
         cls.log_pathes = inference_log
 
@@ -33,8 +44,28 @@ class ScoringService(object):
         """
 
         prediction = input.copy()
-        prediction['weekday'] = pd.to_datetime(prediction['datetime'], format='%Y-%m-%d').apply(lambda x: x.weekday())
-        prediction['prediction'] = prediction['weekday'].apply(lambda x: 1 if x==4 or x==5 else 0)
+        prediction = prediction[['datetime', 'start_code', 'end_code', 'KP']]
+        df_input = input.copy()
+        
+        df_input['weekday'] = pd.to_datetime(df_input['datetime'], format='%Y-%m-%d').apply(lambda x: x.weekday())
+        columns_input = ['start_code', 'end_code', 'KP', 'OCC', 'allCars', 'speed', 'weekday']
+        
+        # model_path = '../model/model.pkl'
+
+        model_input = df_input[columns_input]
+
+        # with open(model_path, 'rb') as f:
+        #     model = pickle.load(f)
+
+
+        y_pred = cls.model.predict(model_input)
+        # print(y_pred)
+        prediction['prediction_speed'] = y_pred
+        # prediction['prediction'] = prediction.eval('prediction_speed < 40')
+        prediction['prediction'] = prediction['prediction_speed'].apply(lambda x: 1 if x < 40.0 else 0)
+        print(prediction['prediction'].sum())
+        # prediction.drop(index=['prediction_speed'], inplace=True)
+        #prediction['prediction'] = prediction['weekday'].apply(lambda x: 1 if x==4 or x==5 else 0)
         prediction = prediction[['datetime', 'start_code', 'end_code', 'KP', 'prediction']]
 
         return prediction
